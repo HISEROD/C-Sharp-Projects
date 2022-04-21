2e4f01aa35146e68c534dab58496038082bb8760
HISEROD <hisebeast@gmail.com>
2022-04-21 00:46:19 -0500

    added color changing fx

--- GasGasGas/GasGasGas/wwwroot/js/site.js
+++ GasGasGas/GasGasGas/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+let elem = document.getElementById('mainheading');
+let hue = 0;
+
+function changeColor() {
+    elem.style.color = `hsl(${hue}, 95%, 50%)`;
+    hue = (hue + 1) % 360;
+    setTimeout(changeColor, 16);
+}
+
+changeColor();
