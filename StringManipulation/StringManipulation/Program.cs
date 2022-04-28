636b884fdf81ff1a0fc930f9942e79dc68a50df9
HISEROD <hisebeast@gmail.com>
2022-04-28 16:09:59 -0500

    added string manipulation project

---
+++ StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Here is the concatenation of \"One \", \"Two \", and \"Three\":");
+            // initialize a string using concatenation
+            string tripleStr = "One " + "Two " + "Three";
+            Console.WriteLine(tripleStr);
+
+            Console.WriteLine("\nHere is the uppercase version of that string:");
+            // make the string uppercase
+            tripleStr = tripleStr.ToUpper();
+            Console.WriteLine(tripleStr);
+
+            Console.WriteLine("\nHere is a StringBuilder built one sentence at a time:");
+
+            // create and append to a StringBuilder object
+            StringBuilder buffStr = new StringBuilder("This is a sentence.");
+            buffStr.Append(" This is a second sentence.");
+            buffStr.Append(" There are two sentences that come before this one.");
+
+            Console.WriteLine(buffStr);
+
+            // Allow user to bask in the presence of the open terminal as it shines forth
+            // or at least allow him to read what's on the screen
+            Console.ReadLine();
+        }
+    }
+}
