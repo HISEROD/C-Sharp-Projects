93fd8d81ebf50a1fc464e18476566d513c2f78cc
HISEROD <hisebeast@gmail.com>
2022-05-03 23:59:03 -0500

    added project: MethodOverloading

---
+++ MethodOverloading/MethodOverloading/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class MyClass
+    {
+        // create a method for MyClass
+        public static int Method1(int integer)
+        {
+            return integer + 2;
+        }
+
+        // overload the method to accept decimals and
+        // strings, performing different operations on each
+        public static int Method1(decimal dec)
+        {
+            return Convert.ToInt32(dec * 2);
+        }
+        public static int Method1(string str)
+        {
+            return Convert.ToInt32(Convert.ToInt32(str) * 99);
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class
+            MyClass Instance1 = new MyClass();
+            // get user input
+            Console.WriteLine("Enter a number to be passed to Method1 as an integer:");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            // call Method1 and pass the user's integer as an argument
+            Console.WriteLine(MyClass.Method1(userInput1));
+
+            // instantiate the class again
+            MyClass Instance2 = new MyClass();
+            // get user input again
+            Console.WriteLine("Enter a number to be passed to Method1 as a decimal:");
+            decimal userInput2 = Convert.ToDecimal(Console.ReadLine());
+            // call Method1 and pass the user's decimal as an argument
+            Console.WriteLine(MyClass.Method1(userInput2));
+
+            // instantiate the class yet again
+            MyClass Instance3 = new MyClass();
+            // get user input yet again
+            Console.WriteLine("Enter a number to be passed to Method1 as a string:");
+            string userInput3 = Console.ReadLine();
+            // call Method1 and pass the user's string as an argument
+            Console.WriteLine(MyClass.Method1(userInput3));
+
+            // Let not the console close so hastily, lest the user see not the fruit thereof.
+            Console.ReadLine();
+        }
+    }
+}
