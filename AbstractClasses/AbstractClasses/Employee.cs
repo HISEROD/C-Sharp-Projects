35856b12650a46f87f21912f7fb04485fcf184f3
HISEROD <hisebeast@gmail.com>
2022-05-04 15:57:40 -0500

    added project: AbstractClasses

---
+++ AbstractClasses/AbstractClasses/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Employee : Person
+    {
+        // create a special version of SayName that starts with "Employee Name:"
+        public override void SayName()
+        {
+            Console.WriteLine("Employee Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,15 +10,18 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Bob" };
+            game.ListPlayers();
             Deck deck = new Deck();
             deck.Shuffle(3);
 
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Rank + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Rank + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
 
