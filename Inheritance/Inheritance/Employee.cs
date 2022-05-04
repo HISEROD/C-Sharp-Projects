d649462c7bd0393ece42f7d424a49a2c1294a869
HISEROD <hisebeast@gmail.com>
2022-05-04 15:23:39 -0500

    added project: Inheritance

---
+++ Inheritance/Inheritance/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    // inherit properties and method from Person class
+    class Employee : Person
+    {
+        // create Id property
+        public int Id { get; set; }
+    }
+}
---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        // create name properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // void method to display the person's name
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
