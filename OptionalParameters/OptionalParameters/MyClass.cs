64664db695df1af36240164415723b3a1a2affba
HISEROD <hisebeast@gmail.com>
2022-05-04 01:47:57 -0500

    added project: OptionalParameters

---
+++ OptionalParameters/OptionalParameters/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class MyClass
+    {
+        // distance function for one or two dimensions
+        public int Method1(int a, int b = 0)
+        {
+            return Convert.ToInt32(Math.Sqrt(a * a + b * b));
+        }
+    }
+}
---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate class
+            MyClass instance = new MyClass();
+            // get input from user
+            Console.WriteLine("Please enter either one or two numbers one at a time:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            // second input stored as a string to prevent exception when converting to int
+            string b = Console.ReadLine();
+
+            // if the second input is empty only pass one argument
+            if (b == "")
+            {
+                Console.WriteLine(instance.Method1(a));
+            }
+            else // pass both arguments when b is provided
+            {
+                Console.WriteLine(instance.Method1(a, Convert.ToInt32(b)));
+            }
+
+            // give the user time to peruse the multitude of pixels before him
+            Console.ReadLine();
+        }
+    }
+}
