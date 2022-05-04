73d8e2565f41cc9676f62680fc0669cae123679b
HISEROD <hisebeast@gmail.com>
2022-05-04 02:01:38 -0500

    added project: VoidMethod

---
+++ VoidMethod/VoidMethod/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class MyClass
+    {
+        // create method which returns no value
+        public void Method(int a, int b)
+        {
+            a = 2 * a;
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class
+            MyClass instance = new MyClass();
+            // multiply 10 by 2 and display 2
+            instance.Method(10, 2);
+            // multiply 9 by 2 and display 5
+            instance.Method(b: 5, a: 9);
+
+            // keep console open
+            Console.Read();
+        }
+    }
+}
