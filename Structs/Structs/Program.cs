4ba9ecd879a3fc25bf8618251f93d4f7336f57fb
HISEROD <hisebeast@gmail.com>
2022-05-04 23:17:50 -0500

    added project: Structs

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate number object
+            Number num = new Number() { Amount = 1.2345678910111213141516171819202122m };
+            // print amount property
+            Console.WriteLine(num.Amount);
+            Console.ReadKey();
+        }
+        struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
