4c5c31b9ee676688a39bff15edd9478acd1cd6c1
HISEROD <hisebeast@gmail.com>
2022-05-06 06:51:26 -0500

    added project: ConstVarAndChainedConstructors

---
+++ ConstVarAndChainedConstructors/ConstVarAndChainedConstructors/ChainedConstuctorClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarAndChainedConstructors
+{
+    class ChainedConstuctorClass
+    {
+        // make a regular constructor that has a parameter for each property
+        public ChainedConstuctorClass(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+        // create a second constructor that automatically assigns 0 to Y
+        public ChainedConstuctorClass(float x) : this(x, 0.0f) { }
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
---
+++ ConstVarAndChainedConstructors/ConstVarAndChainedConstructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarAndChainedConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate a constant float to store the mathematical circle constant tau
+            const float TAU = 6.2831853f;
+            Console.WriteLine("the const TAU equals approximately " + TAU);
+
+            // use implicit typing with the new var keyword!
+            var awesomeVariable = new ChainedConstuctorClass(TAU);
+            Console.WriteLine("the var of type awesomeVariable has an X property equal to " + awesomeVariable.X);
+
+            Console.Read();
+        }
+    }
+}
